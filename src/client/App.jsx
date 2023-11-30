4d71343e33f72ab63595c8da9018f0e1de35c135
BrandonRicca <tulshibrandon@gmail.com>
2023-11-30 13:36:27 -0500

    first commit

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className='App'>
-        <h1>Boilerplate</h1>
+        <h1>VoltVault</h1>
         <img id='comp-img' src='./computer.png'></img>
         <p>Replace the starter code in this template with something cool</p>
         <Login />
