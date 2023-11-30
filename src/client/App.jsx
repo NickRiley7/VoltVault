c4b3b0a08b272250111a6ec4bd9864562f0ae5df
BrandonRicca <tulshibrandon@gmail.com>
2023-11-30 14:37:32 -0500

    added imports and router to app.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import chalk from 'chalk';
 import Login from './components/Login';
+// import Cart from './components/Cart';
+// import Orders from './components/Orders';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-        <h1>VoltVault</h1>
-        <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
-        <Login />
-    </div>
+    <Router>
+      <>
+        <div className='App'>
+          <h1>VoltVault</h1>
+          <img id='comp-img' src='./computer.png'></img>
+          <Login />
+        </div>
+
+        {/* <Routes>
+          
+        </Routes> */}
+
+
+      </>
+    </Router>
   );
 }
 
