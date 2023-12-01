2259a3fdadf3513332580acd4fe8a4096bc6b50a
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-12-01 12:06:31 -0500

    added the get routes to the item.js

--- src/server/db/items.js
+++ src/server/db/items.js
@@ -1,3 +1,45 @@
-const express = require('express');
+const client = require('./client')
+const util = require ('./util.js');
+
+// const express = require('express');
 // const import { v4 as uuidv4 } from 'uuid';
-const app = express();
+
+
+async function getItemID(Id) {
+  try {
+    const {row:[item]} = await client.query (
+      `SELECT * FROM items 
+      WHERE id = $1`,
+      [id]); 
+      return item; 
+    
+  } catch (err) {
+    throw err; 
+  }
+}
+
+async function getItemByName(name) {
+  try {
+    const{ rows: [item] } = await client.query(
+      `SELECT * FROM items
+      WHERE name = $1 
+      `,
+      [name]
+    );
+    return item;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getALLItems () {
+  try {
+    const{ rows: [item] } = await client.query (
+      `SELECT name, price, img 
+      FROM items`
+    );
+    retrun items; 
+  } catch (err){
+    throw err;
+  }
+}
--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -1,12 +1,78 @@
-const db = require('./client');
-const { createUser } = require('./users');
-// const { v4: uuidv4 } = require('uuid');
+const client = require ('./client')
+const { createOrder } = require('./orders')
+const { v4: uuidv4 } = require('uuid');
 
+async function dropTables() {
+  console.log('Dropping All Tables...');
+  // drop all tables, in the correct order
+  try {
+    await  client.query(`
+    DROP TABLE IF EXISTS orderItems;
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS items;
+    DROP TABLE IF EXISTS users;
+  `)
+  } catch (error) {
+    throw error; 
+  }
+}
 
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+    CREATE TABLE users (
+      id INTEGER PRIMARY KEY,
+      username VARCHAR(255),
+      firstName VARCHAR(255),
+      lastName VARCHAR(255),
+      address VARCHAR(255),
+      email VARCHAR(255),
+      password VARCHAR(255),
+      isAdimn BOOL
+      );
+    `)
+    
+    await client.query(`
+    CREATE TABLE items (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      price DECIMAL(10, 2),
+      details TEXT,
+      img VARCHAR (55)
+      category TEXT,
+      stock INT,
+     );
+     
+    `)
+
+    await client.query(`
+    CREATE TABLE orders(
+      id SERIAL PRIMARY KEY,
+      "userId" INTEGER REFERENCES users(id),
+      order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+      order_status VARCHAR(255),
+      order_total DECIMAL(10,2)
+    )`)
+
+    await client.query(`
+    CREATE TABLE orders_products(
+      id SERIAL PRIMARY KEY,
+      order_id INTEGER REFERENCES orders(id),
+      items_id INTEGER REFERENCES items(id),
+      quantirt INTEGER
+    )`)
+  } 
+  catch (err) {
+    console.error("Error building tables!");
+    throw error;
+  }
+}
 
 const users = [
   {
-    id: 1,
+    id: uuidv4(),
     username: 'ross',
     firstName: 'Ross',
     lastName: 'Ritter',
@@ -16,7 +82,7 @@ const users = [
     isAdmin: true,
   },
  {
-    id: 2,
+    id: uuidv4(),
     username: 'example',
     firstName: 'John',
     lastName: 'Doe',
@@ -29,8 +95,8 @@ const users = [
 
 const items = [
   {
-    id: 1,
-    name: 'item 1' ,  
+    id: uuidv4(),
+    name: 'item1',
     price: 19.99,
     details: 'Description for Item 1',
     img: 'https://example.com/item1.jpg',
@@ -39,7 +105,7 @@ const items = [
     stock: 10,
   },
   {
-    id: 2,
+    id: uuidv4(),
     name: 'Item 2',
     price: 29.99,
     details: 'Description for Item 2',
@@ -49,110 +115,58 @@ const items = [
     stock: 15,
   },
 ];
-const orders = [
-  {
-    order_id: 1,
-    user_id: users[0].id,
-    order_date: new Date(),
-    // https://javascript.info/date#:~:text=The%20string%20format%20should%20be,%2C%20minutes%2C%20seconds%20and%20milliseconds.
-    order_status: 'open',
-    order_total: 0,
-    items: [],
-  },
-  {
-    order_id: 2,
-    user_id: users[1].id,
-    order_date: 
-    order_status: 'open',
-    order_total: 0,
-    items: [],
-  },
-];
 
-const orderItems = [
-  { order_id: orders[0].order_id, product_id: items[0].id, quantity: 2 },
-  { order_id: orders[1].order_id, product_id: items[1].id, quantity: 1 },
-];
+async function createInitialOrders () {
+  try{
+    console.log('starting to create orders')
 
+    const ordersToCreate = [
+      {
+        id: uuidv4(),
+        userId: users[0].id,
+        order_status: 'open',
+        order_total: 0,
+        items: [],
+      },
+      {
+        id: uuidv4(),
+        userId: users[1].id,
+        order_status: 'open',
+        order_total: 0,
+        items: [],
+      },
+    ];
 
-const dropTables = async () => {
-  try {
-      await db.query(`
-      DROP TABLE IF EXISTS users;
-      `)
+    const orders = await Promise.all (ordersToCreate.map(order => createOrder (order)));
   }
-  catch(err) {
-      throw err;
+  catch (err) {
+    console.error (err)
   }
-};
-const createTables = async () => {
-  try {
-    await db.query(`
-      DROP TABLE IF EXISTS users, items, orders, order_items;
+}
 
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(100),
-        first_name VARCHAR(100),
-        last_name VARCHAR(100),
-        address VARCHAR(100),
-        email VARCHAR(100),
-        password VARCHAR(100),
-        is_admin BOOLEAN
-      );
 
-      CREATE TABLE items (
-        id SERIAL PRIMARY KEY,
-        name TEXT,
-        price DECIMAL(10, 2),
-        details TEXT,
-        img VARCHAR(55),
-        category TEXT,
-        stock INT
-      );
+// const orderItems = [
+//   { order_id: orders[0].order_id, product_id: items[0].id, quantity: 2 },
+//   { order_id: orders[1].order_id, product_id: items[1].id, quantity: 1 },
+// ];
 
-      CREATE TABLE orders (
-        order_id SERIAL PRIMARY KEY,
-        user_id INTEGER REFERENCES users(id),
-        order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        order_status VARCHAR(50) DEFAULT 'open',
-        order_total DECIMAL(10, 2),
-        items JSONB[]
-      );
-
-      CREATE TABLE order_items (
-        order_id INTEGER REFERENCES orders(order_id),
-        product_id INTEGER REFERENCES items(id),
-        quantity INTEGER,
-        PRIMARY KEY (order_id, product_id)
-      );
-    `);
-
-    await Promise.all(users.map(async (user) => {
-      await createUser({
-        id: user.id,
-        username: user.username,
-      });
-    }));
-
-    console.log('Seed successfully.');
-  } catch (err) {
-    throw err;
-  }
-};
-
-const seedDatabase = async () => {
-  try {
-    await db.connect();
+async function rebuildDB() {
+  try{
+    client.connect();
+    await dropTables();
     await createTables();
-  } catch (err) {
-    throw err;
-  } finally {
-    db.end();
+    await createInitialOrders();
+  } 
+  catch (err) {
+    console.log('Error during rebuildDB')
   }
-};
+  finally {
+    client.end()
+  }
+}
 
-seedDatabase();
+
+rebuildDB()
 // module.exports = { users, items, orders, orderItems };
 
 // need to add the SQL to the User table 
