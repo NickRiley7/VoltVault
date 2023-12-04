8c61cccde06c57db8c5e16470d608436cb5200f4
Nick Waters <nicholas.r.waters@gmail.com>
2023-12-03 22:55:43 -0500

    added getAllUsers route.

--- src/server/db/users.js
+++ src/server/db/users.js
@@ -1,83 +1,129 @@
-const db = require('./client')
-const bcrypt = require('bcrypt');
+const db = require("./client");
+const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
-async function createUser({ username, firstName, lastName, address, email, password, isAdmin }) {
-    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
-    try {
-        const { rows: [user ] } = await db.query(`
+async function createUser({
+  username,
+  firstName,
+  lastName,
+  address,
+  email,
+  password,
+  isAdmin,
+}) {
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+  try {
+    const {
+      rows: [user],
+    } = await db.query(
+      `
         INSERT INTO users(username, firstName, lastName, address, email, password, isAdmin)
-        VALUES($1, $2, $3)
+        VALUES($1, $2, $3, $4, $5, $6, $7)
         ON CONFLICT (email) DO NOTHING
-        RETURNING *`, [username, firstName, lastName, address, email, hashedPassword, isAdmin]);
+        RETURNING *`,
+      [username, firstName, lastName, address, email, hashedPassword, isAdmin]
+    );
 
-        return user;
-    } catch (err) {
-        throw err;
-    }
+    return user;
+  } catch (err) {
+    throw err;
+  }
 }
 
-async function getUser({email, password}) {
-    if(!email || !password) {
-        return;
-    }
-    try {
-        const user = await getUserByEmail(email);
-        if(!user) return;
-        const hashedPassword = user.password;
-        const passwordsMatch = await bcrypt.compare(password, hashedPassword);
-        if(!passwordsMatch) return;
-        delete user.password;
-        return user;
-    } catch (err) {
-        throw err;
-    }
+async function getAllUsers() {
+  try {
+    const { rows: users } = await client.query(`
+        SELECT * FROM users;
+        `);
+    return users;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function getUser({ email, password }) {
+  if (!email || !password) {
+    return;
+  }
+  try {
+    const user = await getUserByEmail(email);
+    if (!user) return;
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) return;
+    delete user.password;
+    return user;
+  } catch (err) {
+    throw err;
+  }
 }
 
 async function getUserByEmail(email) {
-    try {
-        const { rows: [ user ] } = await db.query(`
+  try {
+    const {
+      rows: [user],
+    } = await db.query(
+      `
         SELECT * 
         FROM users
-        WHERE email=$1;`, [ email ]);
+        WHERE email=$1;`,
+      [email]
+    );
 
-        if(!user) {
-            return;
-        }
-        return user;
-    } catch (err) {
-        throw err;
+    if (!user) {
+      return;
     }
+    return user;
+  } catch (err) {
+    throw err;
+  }
 }
 
 async function getUserByUsername(username) {
-    try {
-        const { rows: [ user ] } = await db.query(`
+  try {
+    const {
+      rows: [user],
+    } = await db.query(
+      `
         SELECt *
         FROM users
-        WHERE username=$1;`, [ username ]);
+        WHERE username=$1;`,
+      [username]
+    );
 
-        if(!username) {
-            return;
-        }
-        return user;
-    } catch (error) {
-        throw error;
+    if (!username) {
+      return;
     }
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
+//UPDATE
+async function updateUser() {
+  try {
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+//UPDATE - what order user removed from items?
+// what to do w/ placed orders that already have a user that is to be deleted?
+//
 async function destroyUser(user) {
-    try {
-        await db.query(`
-        DELETE FROM users`)
-    } catch (error) {
-        throw error;
-    }
+  try {
+    await db.query(`
+        DELETE FROM users`);
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
-    createUser,
-    getUser,
-    getUserByEmail,
-    getUserByUsername
+  createUser,
+  getAllUsers,
+  getUser,
+  getUserByEmail,
+  getUserByUsername,
 };
