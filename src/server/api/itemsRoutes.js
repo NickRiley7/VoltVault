b77000cd005d81fbd8594812bb888604ba77c82f
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-12-04 11:20:56 -0500

    added get itemsbyName and post new items to the itemsRoutes  API file

--- src/server/api/itemsRoutes.js
+++ src/server/api/itemsRoutes.js
@@ -30,3 +30,24 @@ router.get('/items/:id', async(req, res, next) => {
   }
 }
 );
+
+router.get('/items/name/:name', async (req, res, next) => {
+  try {
+    const {name} = req.params;
+    const item = await getItemByName(name);
+    res.json(item);
+  } catch (err) {
+    next (err);
+  }
+}
+);
+
+router.post('/items' , async (req, res, next) => {
+  try {
+    const newItem = req.body;
+    const createItem = await createItem(newItem);
+    res.json(createdItem);
+  } catch (err) {
+    next (err)
+  }
+});
