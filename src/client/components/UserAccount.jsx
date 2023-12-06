47982e3ac8fe1fc8c1da2e1a99798c99891c916d
Nick Waters <nicholas.r.waters@gmail.com>
2023-12-06 11:50:17 -0500

    adding fetchOrders

--- src/client/components/UserAccount.jsx
+++ src/client/components/UserAccount.jsx
@@ -9,11 +9,13 @@ function UserAccount() {
 
   useEffect(() => {
     fetchUser();
+    fetchOrders();
   }, []);
 
   async function fetchUser() {
     try {
       let { data } = await axios.get(`${API}/users/1`);
+      console.log(data);
       setFirstName(data.firstname);
       setUsername(data.username);
       setEmail(data.email);
@@ -21,6 +23,26 @@ function UserAccount() {
       console.error(err.message);
     }
   }
+
+  async function fetchOrders() {
+    try {
+      let { data } = await axios.get(`${API}/`);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  // if (!token) {
+  //   return <h2>Please login or create an account.</h2>;
+  // } else {
+  //   return (
+  //     <>
+  //       <h1>Hi, {firstName}</h1>
+  //       <h2>Your Username: {username}</h2>
+  //       <h2>Your Email: {email}</h2>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
