1efe1cb8811a2d2e89dca229ebdb8553e5a527d4
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-12-08 13:59:51 -0500

    functional searchbar

--- src/client/components/SearchBar.jsx
+++ src/client/components/SearchBar.jsx
@@ -1,68 +1,71 @@
 import { useState, useEffect } from "react";
-
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function Searchbar() {
   const [items, setItems] = useState([]);
   const [results, setResults] = useState([]);
   const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
-  
   const fetchItems = async () => {
     let API = "http://localhost:3000/api";
 
     try {
       const response = await axios.get(`${API}/items/`);
       setItems(response.data);
-
-     
-      fetchData(response.data);
     } catch (err) {
       console.error(err.message);
     }
   };
 
-  
-  const fetchData = (items) => {
-    
-    const filteredResults = items.filter((item) => {
-      
-      return item && item.name && item.name.toLowerCase().includes(input.toLowerCase());
-    });
-
-    setResults(filteredResults);
-  };
-
-  
   const handleChange = (value) => {
     setInput(value);
-    
-    fetchData(items);
+    fetchData(value);
   };
 
-  
+  const fetchData = (value) => {
+    if (value === "") {
+      setResults([]);
+    } else {
+      const filteredResults = items.filter((item) => {
+        return item && item.name && item.name.toLowerCase().includes(value.toLowerCase());
+      });
+
+      setResults(filteredResults);
+    }
+  };
+
+  const handleItemClick = (itemId) => {
+    navigate(`/items/${itemId}`);
+  };
+
   useEffect(() => {
     fetchItems();
   }, []);
 
   return (
     <div>
-      
       <div className="input-wrapper">
-        <input placeholder="Search..." value={input} onChange={(e) => handleChange(e.target.value)}/>
+        <input
+          placeholder="Search..."
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
       </div>
 
-  
       {results.length > 0 && (
         <div>
           <ul>
             {results.map((result) => (
-              <li key={result.id}>{result.name}</li>
+              <li key={result.id} onClick={() => handleItemClick(result.id)}>
+                {result.name}
+              </li>
             ))}
           </ul>
         </div>
       )}
-      {results.length === 0 && <p>No item found.</p>}
+      {/* {results.length === 0 && <p>No item found.</p>} */}
     </div>
   );
 }
