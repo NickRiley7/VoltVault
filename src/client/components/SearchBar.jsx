e99207b4877f63339d1cc2688321b570ae962751
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-12-09 18:17:15 -0500

    added onhoover effect on the results of the searchbar

--- src/client/components/SearchBar.jsx
+++ src/client/components/SearchBar.jsx
@@ -6,6 +6,7 @@ function Searchbar() {
   const [items, setItems] = useState([]);
   const [results, setResults] = useState([]);
   const [input, setInput] = useState("");
+  const [hoveredItemId, setHoveredItemId] = useState(null);
   const navigate = useNavigate();
 
   const fetchItems = async () => {
@@ -60,14 +61,17 @@ function Searchbar() {
         <div id="searchCard" className="card">
           <div className="card-body">
             {results.map((result) => (
-              <p className="card-text" key={result.id} onClick={() => handleItemClick(result.id)}>
+              <p
+                className={`card-text search-result-item p-2 mb-1 rounded cursor-pointer ${
+                  hoveredItemId === result.id ? "bg-primary" : ""
+                }`}
+                key={result.id} onClick={() => handleItemClick(result.id)} onMouseEnter={() => setHoveredItemId(result.id)} onMouseLeave={() => setHoveredItemId(null)}>
                 {result.name}
               </p>
             ))}
           </div>
         </div>
       )}
-      {/* {results.length === 0 && <p>No item found.</p>} */}
     </div>
   );
 }
