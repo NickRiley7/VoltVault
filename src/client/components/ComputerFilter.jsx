43fbe84a75c4688fa03f7324510f2eafb2ba8677
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-12-09 21:20:32 -0500

    started the filter for computer component

---
+++ src/client/components/ComputerFilter.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function FilterForComputer () {
+  const [items, setItems] = useState([]);
+  const [category, setCategory] =useState ([]);
+}
