90cfcb71e8755e50611a3d82395f7985a9c4aafa
BrandonRicca <tulshibrandon@gmail.com>
2023-12-10 18:22:26 -0500

    commented out wishlist link

--- src/client/components/Navigation.jsx
+++ src/client/components/Navigation.jsx
@@ -30,7 +30,7 @@ export default function Navigations({ token }) {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link
                 className="nav-link active"
                 aria-current="page"
@@ -39,7 +39,7 @@ export default function Navigations({ token }) {
               >
                 Wishlist
               </Link>
-            </li>
+            </li> */}
             <li className="nav-item dropdown">
               <Link
                 className="nav-link dropdown-toggle"
