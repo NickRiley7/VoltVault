90e498f12352172078625441e5dd72723592f943
Nick Waters <nicholas.r.waters@gmail.com>
2023-12-10 21:01:07 -0500

    conditional rendering of if user is an admin or not.

--- src/client/components/AllUsers.jsx
+++ src/client/components/AllUsers.jsx
@@ -1,16 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 let API = "http://localhost:3000/api";
 
 function AllUsers({ token }) {
   const [users, setUsers] = useState([]);
+  const [decoded, setDecoded] = useState({});
   const Navigate = useNavigate();
 
   useEffect(() => {
     fetchAllUsers();
+    if (token) {
+      setDecoded(jwtDecode(token));
+    }
   }, []);
+
+  console.log(decoded);
 
   async function fetchAllUsers() {
     let { data } = await axios.get(`${API}/users`, {
@@ -20,51 +27,52 @@ function AllUsers({ token }) {
       },
     });
 
-    console.log(data);
     setUsers(data.users);
-    console.log(users);
   }
-
-  return (
-    <>
-      <h1>Registered Users</h1>
-      <table>
-        <thead>
-          <tr>
-            <td>User ID</td>
-            <td>Username</td>
-            <td>First Name</td>
-            <td>Last Name</td>
-            <td>Email</td>
-            <td>Address</td>
-            <td>Address 2</td>
-            <td>City</td>
-            <td>State</td>
-            <td>Zip</td>
-            <td>User Role</td>
-          </tr>
-        </thead>
-        {users.map((user) => {
-          return (
-            <tr key={user.id}>
-              {/* USER ID NEEDS TO BE SET TO UNDERLINE/COLOR DIFFERENT TO INDICATE LINK */}
-              <td onClick={() => Navigate(`/users/${user.id}`)}>{user.id}</td>
-              <td>{user.username}</td>
-              <td>{user.firstname}</td>
-              <td>{user.lastname}</td>
-              <td>{user.email}</td>
-              <td>{user.address}</td>
-              <td>{user.address2}</td>
-              <td>{user.city}</td>
-              <td>{user.state}</td>
-              <td>{user.zip}</td>
-              <td>{user.isadmin ? "Admin" : "User"}</td>
+  if (decoded.isAdmin) {
+    return (
+      <>
+        <h1>Registered Users</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>User ID</td>
+              <td>Username</td>
+              <td>First Name</td>
+              <td>Last Name</td>
+              <td>Email</td>
+              <td>Address</td>
+              <td>Address 2</td>
+              <td>City</td>
+              <td>State</td>
+              <td>Zip</td>
+              <td>User Role</td>
             </tr>
-          );
-        })}
-      </table>
-    </>
-  );
+          </thead>
+          {users.map((user) => {
+            return (
+              <tr key={user.id}>
+                {/* USER ID NEEDS TO BE SET TO UNDERLINE/COLOR DIFFERENT TO INDICATE LINK */}
+                <td onClick={() => Navigate(`/users/${user.id}`)}>{user.id}</td>
+                <td>{user.username}</td>
+                <td>{user.firstname}</td>
+                <td>{user.lastname}</td>
+                <td>{user.email}</td>
+                <td>{user.address}</td>
+                <td>{user.address2}</td>
+                <td>{user.city}</td>
+                <td>{user.state}</td>
+                <td>{user.zip}</td>
+                <td>{user.isadmin ? "Admin" : "User"}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </>
+    );
+  } else {
+    return <h1>You must have admin rights to view this page.</h1>;
+  }
 }
 
 export default AllUsers;
