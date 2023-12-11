132945d670a700b2c49b00fd468c59fd4b06bfa5
tbgamal <tbgamalm@gmail.com>
2023-12-11 13:48:15 -0600

    checkout function is working now. However, fetch the cart is throwing an error after clicking checkout

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -53,21 +53,14 @@ useEffect(() => {
     }
   }
 
+      const totalItemAmount = items.map(item => item.price * item.quantity)
+      console.log(totalItemAmount)
+      const overallTotalAmount = totalItemAmount.reduce((acc, cur) => acc + cur, 0)
+      // console.log (overallTotalAmount)
+      // console.log(cart.id)
+
   async function addItem (item) {
-    // e.prevantDefault()
-    // setQuantity(e.target.value)
     try{
-      // const itemsQuantity = items.map(item => item.quantity)
-      // const currentQuantity = 0
-      // currentQuantity =+ itemsQuantity
-
-      // const { data: orderItem } = await axios.get (`${API}/order_items/${orderItemsId}`)
-
-      // console.log (orderItem)
-      // console.log (orderItem.quantity)
-      // setQuantity(orderItem.quantity)
-
-
       const response = await fetch(`${API}/order_items/${item.orderItemsId}`,
       {
         method: "PATCH",
@@ -92,20 +85,7 @@ useEffect(() => {
   }
 
   async function reduceItem (item) {
-    // e.prevantDefault()
-    // setQuantity(e.target.value)
     try{
-      // const itemsQuantity = items.map(item => item.quantity)
-      // const currentQuantity = 0
-      // currentQuantity =+ itemsQuantity
-
-      // const { data: orderItem } = await axios.get (`${API}/order_items/${orderItemsId}`)
-
-      // console.log (orderItem)
-      // console.log (orderItem.quantity)
-      // setQuantity(orderItem.quantity)
-
-
       const response = await fetch(`${API}/order_items/${item.orderItemsId}`,
       {
         method: "PATCH",
@@ -115,17 +95,55 @@ useEffect(() => {
         },
         body:JSON.stringify({
           quantity: item.quantity-1
-          // need to figure out how to PATCH quantity +1 when adding more item
         })
       })
       let json = await response.json()
-      // setQuantity(json.)
 
       fetchCart()
       
 
     } catch (error) {
       console.error ('error in adding item quantity', error)
+    }
+  }
+
+  async function deleteItem (item) {
+    try{
+      const response = await fetch(`${API}/order_items/${item.orderItemsId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type" : "application/json",
+          "Authorization" : `Bearer ${token}`
+        }
+      })
+      let json = await response.json()
+
+      fetchCart()
+      
+    } catch (error) {
+      console.error ('error in deleting item in cart', error)
+    }
+  }
+
+  async function checkOut(cart) {
+    try {
+      const response = await fetch (`${API}/orders/${cart.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type" : "application/json",
+          "Authorization" : `Bearer ${token}`
+        },
+        body: JSON.stringify ({
+          isOpen: false
+        })
+      })
+      let json = await response.json()
+      console.log (json)
+      fetchCart()
+    }
+    catch (error) {
+      console.error ('error in checking out items', error)
     }
   }
 
@@ -154,6 +172,7 @@ useEffect(() => {
                   <div>quantity {item.quantity}</div>
                   <button onClick={() => addItem(item)}>add item</button>
                   <button onClick={() => reduceItem(item)}>reduce item</button>
+                  <button onClick={() => deleteItem(item)}>DELETE</button>
                   {/* <div>
                     <input 
                       type="number"
@@ -170,7 +189,8 @@ useEffect(() => {
             : 
             <h3>Cart is empty</h3>
         }
-        <h3>TOTAL AMOUNT {totalCart}</h3>
+        <h3>TOTAL AMOUNT {overallTotalAmount}</h3>
+        <button onClick={() => checkOut(cart)}>Check Out</button>
       </>
       // <div>
       //   <h2>Shopping Cart</h2>
@@ -187,7 +207,7 @@ useEffect(() => {
       // </div>
     );
   }
-  else return <h1>This is {token}</h1>
+  else return <h1>You have not item in your cart</h1>
 };
 
 
