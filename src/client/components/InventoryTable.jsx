1ee0f5f9edaff277f3c1c0ad825868bbb2f2d119
Nick Waters <nicholas.r.waters@gmail.com>
2023-12-11 12:04:55 -0500

    rendering all invtentory items to table on page.

--- src/client/components/InventoryTable.jsx
+++ src/client/components/InventoryTable.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 let API = "http://localhost:3000/api";
 
@@ -16,8 +17,47 @@ function InventoryTable({ admin, token }) {
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log(data);
-    // setInventory(data.users);
+    setInventory(data);
+  }
+
+  if (admin) {
+    return (
+      <>
+        <h1>Inventory</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>Category</td>
+              <td>Price</td>
+              <td>Stock</td>
+              <td></td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {inventory.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.category}</td>
+                  <td>{item.price}</td>
+                  <td>{item.stock}</td>
+                  <td>
+                    <Link>Edit</Link>
+                  </td>
+                  <td>
+                    <Link>Delete</Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
   }
 
   return <h1>I'M HOOKED UP!</h1>;
