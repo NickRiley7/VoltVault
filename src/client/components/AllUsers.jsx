22b79fc911d1bbc6a2e58431bf5b010f6fe852b4
Nick Waters <nicholas.r.waters@gmail.com>
2023-12-14 10:28:56 -0500

    scaled back userEdit

--- src/client/components/AllUsers.jsx
+++ src/client/components/AllUsers.jsx
@@ -38,20 +38,11 @@ function AllUsers({ admin, token }) {
 
   async function editUser(id) {
     try {
-      let { data } = await axios.patch(
+      const { data } = await axios.patch(
         `${API}/users/${id}`,
         {
           username,
-          firstname,
-          lastname,
-          email,
-          address,
-          address2,
-          city,
-          state,
-          zip,
           isAdmin,
-          password,
         },
         {
           headers: {
@@ -60,6 +51,7 @@ function AllUsers({ admin, token }) {
           },
         }
       );
+      console.log(data);
       fetchAllUsers();
     } catch (err) {
       console.error(err);
@@ -165,177 +157,6 @@ function AllUsers({ admin, token }) {
                                     onChange={(e) =>
                                       setUsername(e.target.value)
                                     }
-                                  />
-                                </label>
-                                <label
-                                  htmlFor="inputEmail4"
-                                  className="form-label"
-                                >
-                                  Email:{""}
-                                  <input
-                                    type="email"
-                                    className="form-control shadow"
-                                    id="inputEmail4"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                  />
-                                </label>
-                                <label
-                                  htmlFor="inputPassword4"
-                                  className="form-label"
-                                >
-                                  Password:
-                                  <input
-                                    className="form-control shadow"
-                                    id="inputPassword4"
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => {
-                                      setPassword(e.target.value);
-                                    }}
-                                  />
-                                </label>
-                                <label className="form-label">
-                                  First Name:{""}
-                                  <input
-                                    type="text"
-                                    className="form-control shadow"
-                                    value={firstname}
-                                    onChange={(e) =>
-                                      setFirstName(e.target.value)
-                                    }
-                                  />
-                                </label>
-                                <label className="form-label">
-                                  Last Name:{""}
-                                  <input
-                                    type="text"
-                                    className="form-control shadow"
-                                    value={lastname}
-                                    onChange={(e) =>
-                                      setLastName(e.target.value)
-                                    }
-                                  />
-                                </label>
-                                <label
-                                  htmlFor="inputAddress"
-                                  className="form-label"
-                                >
-                                  Address:
-                                  <input
-                                    type="text"
-                                    className="form-control shadow"
-                                    id="inputAddress"
-                                    placeholder="1234 Main St"
-                                    value={address}
-                                    onChange={(e) => setAddress(e.target.value)}
-                                  />
-                                </label>
-                                <label
-                                  htmlFor="inputAddress2"
-                                  className="form-label"
-                                >
-                                  Address 2:
-                                  <input
-                                    type="text"
-                                    className="form-control shadow"
-                                    id="inputAddress2"
-                                    placeholder="Apartment, studio, floor"
-                                    value={address2}
-                                    onChange={(e) =>
-                                      setAddress2(e.target.value)
-                                    }
-                                  />
-                                </label>
-                                <label
-                                  htmlFor="inputCity"
-                                  className="form-label"
-                                >
-                                  City:{""}
-                                  <input
-                                    type="text"
-                                    className="form-control shadow"
-                                    id="inputCity"
-                                    value={city}
-                                    onChange={(e) => setCity(e.target.value)}
-                                  />
-                                </label>
-                                <label
-                                  htmlFor="inputState"
-                                  className="form-label"
-                                >
-                                  State:
-                                  <select
-                                    id="inputState"
-                                    className="form-select shadow"
-                                    value={state}
-                                    onChange={(e) => setState(e.target.value)}
-                                  >
-                                    <option>Select State</option>
-                                    <option value="AL">AL</option>
-                                    <option value="AK">AK</option>
-                                    <option value="AZ">AZ</option>
-                                    <option value="AR">AR</option>
-                                    <option value="CA">CA</option>
-                                    <option value="CO">CO</option>
-                                    <option value="CT">CT</option>
-                                    <option value="DE">DE</option>
-                                    <option value="DC">DC</option>
-                                    <option value="FL">FL</option>
-                                    <option value="GA">GA</option>
-                                    <option value="HI">HI</option>
-                                    <option value="ID">ID</option>
-                                    <option value="IL">IL</option>
-                                    <option value="IN">IN</option>
-                                    <option value="IA">IA</option>
-                                    <option value="KS">KA</option>
-                                    <option value="KY">KY</option>
-                                    <option value="LA">LA</option>
-                                    <option value="ME">ME</option>
-                                    <option value="MD">MD</option>
-                                    <option value="MA">MA</option>
-                                    <option value="MI">MI</option>
-                                    <option value="MN">MN</option>
-                                    <option value="MS">MS</option>
-                                    <option value="MO">MO</option>
-                                    <option value="MT">MT</option>
-                                    <option value="NE">NE</option>
-                                    <option value="NV">NV</option>
-                                    <option value="NH">NH</option>
-                                    <option value="NJ">NJ</option>
-                                    <option value="NM">NM</option>
-                                    <option value="NY">NY</option>
-                                    <option value="NC">NC</option>
-                                    <option value="ND">ND</option>
-                                    <option value="OH">OH</option>
-                                    <option value="OK">OK</option>
-                                    <option value="OR">OR</option>
-                                    <option value="PA">PA</option>
-                                    <option value="RI">RI</option>
-                                    <option value="SC">SC</option>
-                                    <option value="SD">SD</option>
-                                    <option value="TN">TN</option>
-                                    <option value="TX">TX</option>
-                                    <option value="UT">UT</option>
-                                    <option value="VT">VT</option>
-                                    <option value="VA">VA</option>
-                                    <option value="WA">WA</option>
-                                    <option value="WV">WV</option>
-                                    <option value="WI">WI</option>
-                                    <option value="WY">WY</option>
-                                  </select>
-                                </label>
-                                <label
-                                  htmlFor="inputZip"
-                                  className="form-label"
-                                >
-                                  Zip:
-                                  <input
-                                    type="text"
-                                    className="form-control shadow"
-                                    id="inputZip"
-                                    value={zip}
-                                    onChange={(e) => setZip(e.target.value)}
                                   />
                                 </label>
                                 <label className="form-label">
