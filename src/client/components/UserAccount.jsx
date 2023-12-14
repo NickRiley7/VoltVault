c0f956f34266307806c6ef743643e09a879ec0c9
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-12-13 23:48:34 -0500

    fixing acount page

--- src/client/components/UserAccount.jsx
+++ src/client/components/UserAccount.jsx
@@ -1,7 +1,8 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-let API = "http://localhost:3000/api";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+const API = "http://localhost:3000/api";
 
 function UserAccount({ token, admin, user }) {
   const [firstName, setFirstName] = useState("");
@@ -13,8 +14,9 @@ function UserAccount({ token, admin, user }) {
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
   const [zip, setZip] = useState("");
-  const [itemsHistory, setItemsHistory] = useState([])
-  const [ordersHistory, setOrdersHistory] = useState ([])
+  const [order_Id, setOrder_Id] = useState("");
+  const [itemsHistory, setItemsHistory] = useState([]);
+  const [ordersHistory, setOrdersHistory] = useState([]);
 
   useEffect(() => {
     fetchUser();
@@ -39,31 +41,7 @@ function UserAccount({ token, admin, user }) {
         setCity(data.city);
         setState(data.state);
         setZip(data.zip);
-        setOrder_Id(data.order_id)
-      } catch (err) {
-        console.error(err.message);
-      }
-    }
-  }
-
-  async function fetchOrders() {
-    if (token) {
-      try {
-        let { data } = await axios.get(`${API}/users/`, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        setFirstName(data.firstname);
-        setUsername(data.username);
-        setEmail(data.email);
-        setLastName(data.lastname);
-        setAddress(data.address);
-        setAddress2(data.address2);
-        setCity(data.city);
-        setState(data.state);
-        setZip(data.zip);
+        setOrder_Id(data.order_id);
       } catch (err) {
         console.error(err.message);
       }
@@ -72,117 +50,123 @@ function UserAccount({ token, admin, user }) {
 
   async function fetchOrders() {
     try {
-      let response = await fetch (`${API}/orders/complete_orders/${user.id}`,
+      let response = await fetch(
+        `${API}/orders/complete_orders/${user.id}`,
         {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-          }
-      })
-      let orderHistory = await response.json()
-      setOrdersHistory(orderHistory)
-      const itemsInOrder = json.map(order => order.items)
-      setItemsHistory(itemsInOrder)
-    }
-    catch (error) {
-      console.error ('error in fetching order history', error)
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      let orderHistory = await response.json();
+      setOrdersHistory(orderHistory);
+      const itemsInOrder = orderHistory.map((order) => order.items);
+      setItemsHistory(itemsInOrder);
+    } catch (error) {
+      console.error("error in fetching order history", error);
     }
   }
-
 
   if (!token) {
     return (
       <h2>
         Please <Link to="/login">login</Link> or{" "}
-        <Link t="/register">create an account</Link>.
+        <Link to="/register">create an account</Link>.
       </h2>
     );
   } else if (token && admin) {
     return (
-      <>
-      <div id = "admin-account-info">
-        <h1>Hi, {firstName}</h1>
-        <h2>Your Username: {username}</h2>
-        <h2>Your Email: {email}</h2>
-        <h2>Your First Name: {firstName}</h2>
-        <h2>Your Last Name: {lastName}</h2>
-        <h2>Your Address: {address}</h2>
-        <h2>Your Address 2: {address2}</h2>
-        <h2>Your City: {city}</h2>
-        <h2>Your State: {state}</h2>
-        <h2>Your Zipcode: {zip}</h2>
-      </div>  
-        <form>
-          <label>
-            {" "}
-            User
-            <input type="radio" name="adminAccess" value="user" />
-          </label>
-          <label>
-            {" "}
-            Admin
-            <input type="radio" name="adminAccess" value="Admin" />
-          </label>
-        </form>
-        {
-          ordersHistory.length ? 
-            ordersHistory.map (order => { 
-              return (
-                <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div id="admin-account-info">
+              <h1>Hi, {firstName}</h1>
+              <h2>Your Username: {username}</h2>
+              <h2>Your Email: {email}</h2>
+              <h2>Your First Name: {firstName}</h2>
+              <h2>Your Last Name: {lastName}</h2>
+              <h2>Your Address: {address}</h2>
+              <h2>Your Address 2: {address2}</h2>
+              <h2>Your City: {city}</h2>
+              <h2>Your State: {state}</h2>
+              <h2>Your Zipcode: {zip}</h2>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <form>
+              <label>
+                User
+                <input type="radio" name="adminAccess" value="user" />
+              </label>
+              <label>
+                Admin
+                <input type="radio" name="adminAccess" value="Admin" />
+              </label>
+            </form>
+            {ordersHistory.length ? (
+              ordersHistory.map((order) => (
+                <div key={order.id}>
                   <div>Order ID: {order.id}</div>
                   <div>Order Date: {order.order_date.slice(0, 10)}</div>
                   <div>Order Total: {order.order_total}</div>
                 </div>
-              )
-            })
-            :
-            <h2>no order history</h2>
-        }
-      </>
+              ))
+            ) : (
+              <h2>no order history</h2>
+            )}
+          </div>
+        </div>
+      </div>
     );
   } else {
     return (
-      
-        <div id = "background">
-          <div id = "account-info">
-            <h1>Hi, {firstName}</h1>
-            <h2>Your Username: {username}</h2>
-            <h2>Your Email: {email}</h2>
-            <h2>Your First Name: {firstName}</h2>
-            <h2>Your Last Name: {lastName}</h2>
-            <h2>Your Address: {address}</h2>
-            <h2>Your Address 2: {address2}</h2>
-            <h2>Your City: {city}</h2>
-            <h2>Your State: {state}</h2>
-            <h2>Your Zipcode: {zip}</h2>
-          </div>
-      
-        <div className="col-11 col-sm-11 col-md-11 col-lg-11 m-5 "> 
-      {
-          ordersHistory.length ? 
-            ordersHistory.map (order => { 
-              return (
-              <div id ="order-history">
-                <div className="table-responsive">
-                  <table className= "table-striped shadow table-hover table-light">
-                    <thead>
-                      <tr>
-                        <th scope="col">Order ID: {order.id}</th>
-                        <th scope="col">Order Date: {order.order_date.slice(0, 10)}</th>
-                        <th scope="col">Order Total: {order.order_total}</th>
-                      </tr>
-                    </thead>
-                  </table>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div id="background">
+              <div id="account-info">
+                <h1>Hi, {firstName}</h1>
+                <h2>Your Username: {username}</h2>
+                <h2>Your Email: {email}</h2>
+                <h2>Your First Name: {firstName}</h2>
+                <h2>Your Last Name: {lastName}</h2>
+                <h2>Your Address: {address}</h2>
+                <h2>Your Address 2: {address2}</h2>
+                <h2>Your City: {city}</h2>
+                <h2>Your State: {state}</h2>
+                <h2>Your Zipcode: {zip}</h2>
               </div>
-              )
-            })
-            :
-            <h2>no order history</h2>
-        }
-     </div>
-     </div> 
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="col-11 col-sm-11 col-md-11 col-lg-11 m-5">
+              {ordersHistory.length ? (
+                ordersHistory.map((order) => (
+                  <div key={order.id} id="order-history">
+                    <div className="table-responsive">
+                      <table className="table-striped shadow table-hover table-light">
+                        <thead>
+                          <tr>
+                            <th scope="col">Order ID: {order.id}</th>
+                            <th scope="col">
+                              Order Date: {order.order_date.slice(0, 10)}
+                            </th>
+                            <th scope="col">Order Total: {order.order_total}</th>
+                          </tr>
+                        </thead>
+                      </table>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <h2>no order history</h2>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
